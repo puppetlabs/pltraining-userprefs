658fafa120a36e18b38a1504351bcce2d84dc967
Joseph Oaks <joseph.oaks@puppet.com>
2021-02-12 15:29:32 -0700

    update for rspec fix

--- spec/classes/gitconfig_spec.rb
+++ spec/classes/gitconfig_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 describe 'userprefs::gitconfig' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      let(:facts) { os_facts }
+
       case os_facts[:osfamily]
-      when 'CentOS'
+      when 'RedHat'
         it { is_expected.to compile }
       end
     end
