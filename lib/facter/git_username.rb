bef13622928926c24341f9a70648d701b1444a12
Joseph Oaks <joseph.oaks@puppet.com>
2021-03-24 06:39:07 -0600

    update version #, update to custom fact

--- lib/facter/git_username.rb
+++ lib/facter/git_username.rb
@@ -1,4 +1,4 @@
 Facter.add("git_username") do
   confine :kernel => 'Linux'
-  setcode "/bin/awk '/name/{print $NF}' /home/centos/.gitconfig"
+  setcode "/bin/awk -F=\  '/name/{print $NF}' /home/centos/.gitconfig"
 end
