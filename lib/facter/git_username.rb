9973de3318ced5581e10623d800efe4b5aa15d3c
Joseph Oaks <joseph.oaks@puppet.com>
2021-03-31 08:46:58 -0600

    update for newer ruby 1.9 hash syntax

--- lib/facter/git_username.rb
+++ lib/facter/git_username.rb
@@ -1,4 +1,6 @@
-Facter.add("git_username") do
-  confine :kernel => 'Linux'
+Facter.add(:git_username) do
+  confine :kernel do |value|
+    value == 'Linux'
+  end
   setcode "/bin/awk -F=\  '/name/{print $NF}' /home/centos/.gitconfig"
 end
